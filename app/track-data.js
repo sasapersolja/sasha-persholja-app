500e0da9659b5858b6dd790c3cf9b4437091323c
Sasha Persholja <73987335+sasapersolja@users.noreply.github.com>
2026-08-13 17:46:54 +0200

    Add configurable track data

---
+++ app/track-data.js
@@ -0,0 +1,55 @@
+window.SASHA_TRACK = {
+  artist: "Sasha Persholja",
+  title: "Big Black Puppet",
+  subtitle: "Blues rock, funk and alternative music with Fejkavangard power.",
+  featuredLabel: "FEATURED RELEASE",
+  statement: "Dark. Real. Blues Rock Funk.",
+  contactEmail: "sasha.persholja@gmail.com",
+  website: "https://sashapersholja.com/",
+  media: {
+    poster: "",
+    video: ""
+  },
+  platforms: [
+    {
+      id: "bandcamp",
+      label: "Bandcamp",
+      action: "Buy / Support",
+      note: "Direct from the artist",
+      url: "https://sashapersholja.bandcamp.com/track/sasha-persholja-big-black-puppet"
+    },
+    {
+      id: "apple",
+      label: "Apple Music",
+      action: "Listen",
+      note: "Stream & Download",
+      url: "https://music.apple.com/si/song/big-black-puppet/982227462"
+    },
+    {
+      id: "amazon",
+      label: "Amazon Music",
+      action: "Listen",
+      note: "Stream & Download",
+      url: "https://music.amazon.co.uk/tracks/B00VNAROZU"
+    },
+    {
+      id: "spotify",
+      label: "Spotify",
+      action: "Listen",
+      note: "Stream on Spotify",
+      url: "https://open.spotify.com/track/2pIdN8tXCmOSbnNmYLfQDe"
+    },
+    {
+      id: "youtube",
+      label: "YouTube Music",
+      action: "Listen",
+      note: "Stream on YouTube",
+      url: "https://music.youtube.com/watch?v=3VD7p3k7EPQ&list=OLAK5uy_mm2sYLMXfEzinVeoVPefhK3CbtJ1HcVGM"
+    }
+  ],
+  socials: {
+    instagram: "https://www.instagram.com/sasha.persholja/",
+    youtube: "https://www.youtube.com/@mkudmkud",
+    facebook: "https://www.facebook.com/SashaPersholja/"
+  }
+};
